982c9dbcf35a2c3b6098c847729f580833eaa954
Yakov Golovin <yakov.golovin@gmail.com>
2018-11-11 17:33:46 +0100

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ultra-app';
+
+
+  ngOnInit() {
+    document.body.classList.add('bg-img');
+  }
 }
+
+
